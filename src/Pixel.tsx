7a8c9df27843b1c12378e4ef2f0726c155648f5b
ossan-engineer <bodega1123@gmail.com>
2019-12-22 19:58:08 +0900

    Add Pixel

---
+++ src/Pixel.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  value: number;
+  size: number;
+}
+
+const Pixel = ({ value, size }: Props) => {
+  return <Wrapper value={value} size={size} />;
+};
+
+const Wrapper = styled.div<Props>`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  margin: ${({ size }) => size / 4}px;
+  border-radius: 50%;
+  ${({ value, size }) =>
+    value === 1 &&
+    css`
+      background-color: #fff;
+      box-shadow: 0 0 ${(size / 3) * 2}px #fff, 0 0 ${size}px #f00,
+        inset 0 0 ${(size / 3) * 2}px #fff, inset 0 0 ${size / 3}px #f00;
+    `}
+`;
+
+export default Pixel;
