0232361b2d1582ee972424c7a96cfd8359eb645d
ossan-engineer <bodega1123@gmail.com>
2019-12-22 19:58:37 +0900

    Add App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 
+import Pixels from './Pixels';
+
+const DATA = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 const App: React.FC = () => {
-  return <div>TEST</div>;
+  return (
+    <>
+      <GlobalStyle> </GlobalStyle>
+      <Wrapper>
+        <Pixels data={DATA} size={128}></Pixels>
+        <Pixels data={DATA} size={128}></Pixels>
+        <Pixels data={DATA} size={128}></Pixels>
+      </Wrapper>
+    </>
+  );
 };
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: #000;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 768px;
+  display: flex;
+  /* border: 1px solid #fff; */
+`;
 
 export default App;
